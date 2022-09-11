05f1dee79ef93f5d322fe5128a08a17d229f9718
poly000 <1348292515@qq.com>
2022-09-11 15:05:46 +0800

    update

---
+++ readme.md
@@ -0,0 +1,5 @@
+# fibonacci
+
+simple concurrent & blazingly fast high precision fibonacci calculate program.
+
+thanks to rayon's `join` and `rust-gmp`'s thread safe gmp wrapper.
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
+use std::ops::{Mul, Sub};
+
+use gmp::mpz::Mpz;
 use rayon::join;
 
-fn fib(x: u64) -> u64 {
-    if x < 3 {
-        1 as u64
+fn fib(x: Mpz) -> Mpz {
+    if x < Mpz::from(3) {
+        Mpz::from(1)
     } else {
-        if x % 2 == 1 {
+        if !x.is_multiple_of(&Mpz::from(2)) {
             let n = (x + 1) / 2;
-            let (a, b) = join(|| fib(n).pow(2), || fib(n - 1).pow(2));
+            let m = n.clone();
+            let (a, b) = join(|| fib(m).pow(2), || fib(n - 1).pow(2));
             a + b
         } else {
             let n = x / 2;
-            let (a, b) = join(|| fib(n), || fib(n - 1));
-            (2 * b + a) * a
+            let m = n.clone();
+            let (a, b) = join(|| fib(m), || fib(n.sub(&Mpz::from(1))));
+            (b.mul(Mpz::from(2)) + &a) * a
         }
     }
 }
 
 fn main() {
-    let times = std::env::args().nth(1).unwrap().parse().unwrap();
+    let n = std::env::args().nth(1).unwrap().parse().unwrap();
 
-    for i in 1..=times {
-        let result = fib(i);
-        println!("{i:>4}: {result}");
-    }
+    let result = fib(n);
+    println!("{result}");
 }
